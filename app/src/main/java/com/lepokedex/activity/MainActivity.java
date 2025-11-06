8a988329a99f0e8b99771adb3c53a7adfb81cfe2
rafald10 <rafalazarodiaz@gmail.com>
2025-11-06 09:59:49 +0100

    Creación de función para que el Spinner filtre por tipos

--- app/src/main/java/com/lepokedex/activity/MainActivity.java
+++ app/src/main/java/com/lepokedex/activity/MainActivity.java
@@ -22,6 +22,8 @@ import com.lepokedex.R;
 import com.lepokedex.adapter.PokemonAdapter;
 import com.lepokedex.model.Pokemon;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     ArrayAdapter<Pokemon> adapterDefaultPokemon;
 
@@ -36,10 +38,44 @@ public class MainActivity extends AppCompatActivity {
         // De momento importamos la listView para que aparezcan los Pokémon y el botón de añadir Pokémon
         ListView lvPokemons = findViewById(R.id.lvPokemons);
         Button botonAnadirPokemon = findViewById(R.id.botonAnadirPokemon);
+        Button botonFiltrar = findViewById(R.id.botonFiltrar);
+        Spinner spinnerFiltrar = findViewById(R.id.spinnerFiltrar);
 
         // Creamos el adapter para esta pantalla con la lista de Pokémons y la establecemos a lvPokemons
         adapterDefaultPokemon = new PokemonAdapter(MainActivity.this, 0 , listaPokemons);
         lvPokemons.setAdapter(adapterDefaultPokemon);
+
+        String[] tiposPokemon = { "Todos", "Fuego", "Agua", "Electrico", "Planta", "Normal" };
+        ArrayAdapter<String> adapterSpinner = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, tiposPokemon);
+        adapterSpinner.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerFiltrar.setAdapter(adapterSpinner);
+
+        botonFiltrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Recogemos el valor seleccionado en el Spinner
+                String tipoSeleccionado = spinnerFiltrar.getSelectedItem().toString();
+
+                // Comprobamos la seleccion del usuario
+                if (tipoSeleccionado.equalsIgnoreCase("Todos")) {
+                    lvPokemons.setAdapter(adapterDefaultPokemon);
+                } else {
+                    ArrayList<Pokemon> listaFiltrada = new ArrayList<>();
+
+                    for (Pokemon p : listaPokemons) {
+                        if (p.getTipo().equalsIgnoreCase(tipoSeleccionado)) {
+                            listaFiltrada.add(p);
+                        }
+                    }
+
+                    // Creación de un Adapter con los pokemons filtrados para la lista
+                    ArrayAdapter<Pokemon> adapterFiltrado = new PokemonAdapter(MainActivity.this, 0, listaFiltrada);
+
+                    // Le asignamos el adapter nuevo a la lista de Pokemons
+                    lvPokemons.setAdapter(adapterFiltrado);
+                }
+            }
+        });
 
         // En el setOnClickListener del boton de añadir pokemon creamos un intent que vaya la vista de CreateActivity
         botonAnadirPokemon.setOnClickListener(new View.OnClickListener() {
@@ -69,6 +105,8 @@ public class MainActivity extends AppCompatActivity {
             adapterDefaultPokemon.notifyDataSetChanged();
         }
     }
+
+
 
 
 }
